83816917c4d864ce0d8937b1dc7e928609bd6fa9
penicillin0 <ayahitonakamura@gmail.com>
2022-08-15 18:30:44 +0900

    tweak

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import data from "../data/data.json";
+import data from "../data/member.json";
 import { Team } from "./components/Team";
 
 render(<Team members={data} />, document.getElementById("root"));
